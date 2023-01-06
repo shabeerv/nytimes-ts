f9e351c2d4788a8759a5211442ad7ecdda371d38
shabeerv <shabrv@gmail.com>
2023-01-06 12:14:17 +0400

    improved store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,7 @@
-import { AnyAction, combineReducers, configureStore } from "@reduxjs/toolkit";
-import userReducer from "../reducers/userReducer";
+import { configureStore } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from "redux-persist";
-import statusReducer from "../reducers/statusReducer";
-import errorReducer from "../reducers/errorReducer";
-import newsReducer from "../reducers/newsReducer";
-import { actionTypes as userActions } from "../actions/userAction";
-import commentsReducer from "../reducers/commentsReducer";
+import { rootReducer } from "../reducers/rootReducer";
 
 const persistConfig = {
   key: "root",
@@ -14,30 +9,16 @@ const persistConfig = {
   blacklist: ["status", "error"],
 };
 
-const combinedReducer = combineReducers({
-  user: userReducer,
-  status: statusReducer,
-  error: errorReducer,
-  news: newsReducer,
-  comments: commentsReducer,
-});
-
-const rootReducer = (state: any, action: AnyAction) => {
-  if (action.type === userActions.LOGOUT) {
-    state = undefined;
-  }
-  return combinedReducer(state, action);
-};
-
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
-
 export type AppDispatch = typeof store.dispatch;
-
 export const persistor = persistStore(store);
